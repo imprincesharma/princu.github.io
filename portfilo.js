1942eef1bec1be9007ad17f3fe8a9a4d4eeb6f4c
Prince Bhardwaj <112656783+imprincesharma@users.noreply.github.com>
2023-02-15 16:17:02 +0530

    Add files via upload

---
+++ portfilo.js
@@ -0,0 +1,32 @@
+console.log("this is my portfilo website");
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-content");
+
+function opentab(tabname){
+
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+
+}
+
+
+var sidemeu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemeu.style.right=""
+}
+function closemenu(){
+    sidemeu.style.right="-200px"
+}
+
+
+
+
+
+
